afa7dc15fe4c980027ab4fd011684a0c6934ccee
nicocavi <nico_cavi@hotmail.com>
2020-05-10 15:12:34 -0300

    index

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+window.addEventListener('scroll',()=>{
+    let navBar = document.querySelector('#nav-bar');
+    let section = document.querySelector('#section--baner');
+    let posicion = section.getBoundingClientRect().top;
+    let logo = document.querySelector("#img--logo");
+
+    if(posicion < 0){
+        navBar.style.background = "rgba(0,0,0,0.9)";
+        logo.style.width = "55px";
+    }else{
+        logo.style.width = "70px";
+        navBar.style.background = "rgba(255,255,255,0)";
+    }
+
+});
